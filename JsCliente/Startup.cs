dfe2cea19c96ee08694653b9eeabeac9191e5ca0
margaritav98 <mivillafanep@unincca.edu.co>
2019-04-23 23:23:34 -0500

    JavaScript Client

---
+++ JsCliente/Startup.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace JsCliente
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvcCore()
+                .AddAuthorization()
+                .AddJsonFormatters();
+            services.AddAuthentication("Bearer")
+                .AddJwtBearer(options =>
+                {
+                    options.Authority = "http://localhost:5000";
+                    options.RequireHttpsMetadata = false;
+                    
+                });
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("defaullt", policy =>
+                {
+                    policy.WithOrigins("http://localhost:5003")
+                    .AllowAnyHeader()
+                    .AllowAnyMethod();
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseCors("default");
+            app.UseAuthentication();
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+        }
+    }
+}
